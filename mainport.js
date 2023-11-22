ecdda30e2be9dd0596d7215bac80cd22af04e07c
Sammmy-dev <150556970+Sammmy-dev@users.noreply.github.com>
2023-11-22 18:30:31 +0100

    Update mainport.js

--- mainport.js
+++ mainport.js
@@ -10,31 +10,34 @@ let navigateToPort2 = document.querySelector(`nav ul li:nth-of-type(1)`);
 let navigateToFooter2 = document.querySelector(`nav ul li:nth-of-type(2)`);
 let port =  document.getElementById("port");
 
-menuBtn.addEventListener(`click`, ()=>{
-    sideMenu.classList.toggle("reveal")
-    bar1.classList.toggle("slantback")
-    bar2.classList.toggle("middlebar")
-    bar3.classList.toggle("slantfront")
-})
-
-menuOption.forEach((item)=>(
-    item.addEventListener(`click`, ()=>{
-        sideMenu.classList.remove("reveal")
-        bar1.classList.remove("slantback")
-        bar2.classList.remove("middlebar")
-        bar3.classList.remove("slantfront")
+window.onload = function(){
+    menuBtn.addEventListener(`click`, ()=>{
+        sideMenu.classList.toggle("reveal")
+        bar1.classList.toggle("slantback")
+        bar2.classList.toggle("middlebar")
+        bar3.classList.toggle("slantfront")
     })
-))
+    
+    menuOption.forEach((item)=>(
+        item.addEventListener(`click`, ()=>{
+            sideMenu.classList.remove("reveal")
+            bar1.classList.remove("slantback")
+            bar2.classList.remove("middlebar")
+            bar3.classList.remove("slantfront")
+        })
+    ))
+    
+    navigateToPort.addEventListener(`click`, ()=>{
+        window.scrollTo(0, document.getElementById('port').offsetTop)
+    })
+    navigateToFooter.addEventListener(`click`, ()=>{
+        window.scrollTo(0, document.getElementById('footer').offsetTop)
+    })
+    navigateToPort2.addEventListener(`click`, ()=>{
+        window.scrollTo(0, document.getElementById('port').offsetTop)
+    })
+    navigateToFooter2.addEventListener(`click`, ()=>{
+        window.scrollTo(0, document.getElementById('footer').offsetTop)
+    })
+}
 
-navigateToPort.addEventListener(`click`, ()=>{
-    window.scrollTo(0, document.getElementById('port').offsetTop)
-})
-navigateToFooter.addEventListener(`click`, ()=>{
-    window.scrollTo(0, document.getElementById('footer').offsetTop)
-})
-navigateToPort2.addEventListener(`click`, ()=>{
-    window.scrollTo(0, document.getElementById('port').offsetTop)
-})
-navigateToFooter2.addEventListener(`click`, ()=>{
-    window.scrollTo(0, document.getElementById('footer').offsetTop)
-})
